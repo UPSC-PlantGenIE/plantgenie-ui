946f5b70645bf9ad7ab94c66bf0ae29c3bd75c4d
Jamie McCann <jamie.mccann@umu.se>
2025-02-07 20:58:34 +0100

    added min width to app, updated layout to have main children

--- src/components/layouts/BaseLayout.tsx
+++ src/components/layouts/BaseLayout.tsx
@@ -1,12 +1,25 @@
+import { ReactNode } from "react";
 import styles from "./BaseLayout.module.css";
 
-export const BaseLayout = () => {
+interface BaseLayoutProps {
+  children?: ReactNode;
+}
+
+export const BaseLayout = ({ children }: BaseLayoutProps) => {
   return (
     <>
-      <div id="nav" role="navigation" className={styles.nav}>Nav</div>
-      <div id="header" className={styles.header}>Header</div>
-      <div id="main" className={styles.main}>Main content</div>
-      <div id="footer" className={styles.footer}>Footer</div>
+      <div id="nav" role="navigation" className={styles.nav}>
+        Nav
+      </div>
+      <div id="header" className={styles.header}>
+        Header
+      </div>
+      <div id="main" className={styles.main}>
+        {children ? children : "Main Content"}
+      </div>
+      <div id="footer" className={styles.footer}>
+        Footer
+      </div>
     </>
   );
 };

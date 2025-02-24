279ae07930b7366f522fbcf8688abf2e61a408d9
Jamie McCann <jamie.mccann@umu.se>
2025-02-24 10:51:38 +0100

    Cleaning up gene lists route

--- src/pages/GeneLists.tsx
+++ src/pages/GeneLists.tsx
@@ -1,5 +1,7 @@
 import { MouseEvent, MouseEventHandler } from "react";
+
 import styles from "./GeneLists.module.css";
+
 import { useAppStore } from "../lib/state";
 
 export const GeneListsRoute = () => {
@@ -21,59 +23,31 @@ export const GeneListsRoute = () => {
     );
   };
 
-  // return (
-  //   <div className={styles.GeneLists}>
-  //     {/* <div className={styles.tableWrapper}> */}
-  //       <table>
-  //         <thead>
-  //           <tr>
-  //             <th>id</th>
-  //             <th>name</th>
-  //             <th>creation date</th>
-  //             <th>last updated?</th>
-  //             <th>last accessed?</th>
-  //           </tr>
-  //         </thead>
-  //         <tbody>
-  //           {geneLists.length > 0
-  //             ? geneLists.map((value, index) => (
-  //                 <tr key={index}>
-  //                   <td>{value.id}</td>
-  //                   <td>{value.name}</td>
-  //                   <td>{value.createdAt}</td>
-  //                   <td>{value.updatedAt}</td>
-  //                   <td>{value.lastAccessed}</td>
-  //                 </tr>
-  //               ))
-  //             : null}
-  //         </tbody>
-  //       </table>
-  //     {/* </div> */}
-  //   </div>
-  // );
-
-  return (    <div className={styles.tableWrapper}>
-    <table className={styles.tableElement}>
-      <thead>      <tr>
-        <th>id</th>
-        <th>name</th>
-        <th>creation date</th>
-        <th>last updated</th>
-        <th>last accessed?</th>
-      </tr></thead>
-      <tbody>
-        <tr>
-          <td>
-            1
-          </td>
-          <td>
-            my gene list
-          </td>
-          <td>dkfjsdklfjsdl</td>
-          <td>dsfdsfdfdsfdvcnxvbncbcxvbxcnmvbcxmnbvxcmnvbxcnmvbxmnvbxcnvcbxnvbcxmnvbxcmvnbvnmcxbvmnxbvcnmvbnmvbxcm</td>
-          <td>sdfdsfdsfsdf</td>
-        </tr>
-      </tbody>
-    </table>
-    </div>)
+  return (
+    <div className={styles.tableWrapper}>
+      <table className={styles.tableElement}>
+        <thead>
+          {" "}
+          <tr>
+            <th>id</th>
+            <th>name</th>
+            <th>creation date</th>
+            <th>last updated</th>
+            <th>last accessed?</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>my gene list</td>
+            <td>dkfjsdklfjsdl</td>
+            <td>
+              dsfdsfdfdsfdvcnxvbncbcxvbxcnmvbcxmnbvxcmnvbxcnmvbxmnvbxcnvcbxnvbcxmnvbxcmvnbvnmcxbvmnxbvcnmvbnmvbxcm
+            </td>
+            <td>sdfdsfdsfsdf</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };

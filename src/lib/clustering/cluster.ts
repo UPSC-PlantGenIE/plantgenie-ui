1038e1768d0471a9608a036b29249aa03fe3a972
Jamie McCann <jamie.mccann@umu.se>
2025-03-13 17:00:49 +0100

    adding clustering lib code from old repo

---
+++ src/lib/clustering/cluster.ts
@@ -0,0 +1,40 @@
+import { HierarchicalClusteringArgs, Node } from "./types";
+import { DistanceMetrics } from "./metrics";
+import { LinkageMetrics } from "./linkages";
+import {
+  findNodesToMerge,
+  ladderizeTree,
+  preorderLeafTraversal,
+} from "./utils";
+
+export const hierarchicalClustering = ({
+  data = [],
+  distanceMetric = "euclidean",
+  linkageMetric = "ward",
+}: HierarchicalClusteringArgs): number[] => {
+  // clustering is meaningless or trivial if 0 <= data.length < 2
+  if (data.length < 2) return data.length === 0 ? [] : [0];
+
+  const distance = DistanceMetrics[distanceMetric];
+  const linkage = LinkageMetrics[linkageMetric];
+
+  const distances = data.map((row) =>
+    data.map((otherRow) => distance(row, otherRow))
+  );
+
+  const nodes: Node[] = data.map((_, index) => ({
+    id: index,
+    parent: null,
+    children: [],
+    height: 0,
+    indices: [index],
+  }));
+
+  const root = nodes.reduce((remainingNodes) => {
+    return findNodesToMerge(remainingNodes, data, distances, linkage);
+  }, nodes)[0];
+
+  ladderizeTree(root);
+
+  return preorderLeafTraversal(root);
+};

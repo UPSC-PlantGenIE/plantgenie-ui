857eabcbc0054573639df65e32180cf8bcad45df
Jamie McCann <jamie.mccann@umu.se>
2025-04-06 16:28:14 +0200

    finished up styling for gene list routes, made brand logo smaller with less padding

--- src/pages/GeneLists/gene-search/GeneSearch.tsx
+++ src/pages/GeneLists/gene-search/GeneSearch.tsx
@@ -1,14 +1,19 @@
 import styles from "./GeneSearch.module.css";
 import { Form } from "../../../components/routing/Form";
-import { ChangeEvent, FormEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useRef, useState } from "react";
 import {
   AnnotationsRequest,
   AnnotationsResponse,
   post,
 } from "../../../lib/api";
 import { useAppStore } from "../../../lib/state";
+import { Divider } from "../../../components/general/Divider";
 
 export const GeneSearchRoute = () => {
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const [selectedFileName, setSelectedFileName] = useState(
+    "Attach a file (max 1MB)"
+  );
   const selectedSpecies = useAppStore((state) => state.species);
   const setSearchResults = useAppStore((state) => state.setSearchResults);
   const delimiters: Record<string, string> = {
@@ -16,6 +21,12 @@ export const GeneSearchRoute = () => {
     comma: ",",
     tab: "\t",
     space: " ",
+  };
+  const delimitersForHint: Record<string, string> = {
+    "\n": "new lines",
+    ",": "commas",
+    "\t": "tabs",
+    " ": "spaces",
   };
   const [enteredGeneIds, setEnteredGeneIds] = useState<string>("");
   const [delimiter, setDelimiter] = useState<string>("\n");
@@ -50,13 +61,46 @@ export const GeneSearchRoute = () => {
     );
   };
 
+  const handleFileChange = async (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    console.log(e.target.files?.length);
+    if (e.target.files !== null && e.target.files.length === 1) {
+      const attachedFile = e.target.files[0];
+
+      if (attachedFile.size > 2 ** 20) {
+        setSelectedFileName("File size too big, attach another");
+        return;
+      }
+
+      console.log(e.target.files[0].name);
+      console.log(e.target.files[0]);
+
+      attachedFile.text().then((value) => {
+        setEnteredGeneIds(value.trim());
+        setSelectedFileName(attachedFile.name);
+      });
+    } else {
+      setSelectedFileName("Attach a file");
+    }
+  };
+
+  const handleFileSelectorClick = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
   return (
     <div
       id="container"
       style={{ display: "flex", flexDirection: "column", height: "100%" }}
     >
-      <span>Gene ID Search</span>
-      <Form action="/" method="POST" handleSubmit={handleSubmit} className={styles.geneSearchForm}>
+      <Form
+        action="/"
+        method="POST"
+        handleSubmit={handleSubmit}
+        className={styles.geneSearchContainer}
+      >
         <div
           style={{
             display: "flex",
@@ -114,15 +158,68 @@ export const GeneSearchRoute = () => {
           </label>
         </div>
 
-        <textarea
-          id="gene-ids-entry"
-          placeholder="Enter your gene ids here..."
-          value={enteredGeneIds}
-          onChange={(event: ChangeEvent<HTMLTextAreaElement>) =>
-            setEnteredGeneIds(event.target.value)
-          }
-        ></textarea>
-        <button type="submit">Submit</button>
+        <div id="gene-search-form-container" className={styles.geneSearchFormContainer}>
+          <label
+            style={{
+              fontStyle: "var(--inter)",
+              display: "flex",
+              flexDirection: "column",
+              fontSize: "0.75rem",
+              fontWeight: "bold",
+              alignItems: "flex-start",
+              paddingTop: "0.5em",
+              paddingBottom: "0.5em",
+              paddingLeft: "0.25em",
+            }}
+          >
+            Gene IDs
+          </label>
+          <Divider />
+          <textarea
+            className={styles.geneIdsInput}
+            placeholder={`Enter gene ids delimited by ${delimitersForHint[delimiter]} (Max 1MB)`}
+            value={enteredGeneIds}
+            onChange={(event: ChangeEvent<HTMLTextAreaElement>) =>
+              setEnteredGeneIds(event.target.value)
+            }
+          ></textarea>
+          <div className={styles.fileContainer}>
+            <div
+              id="file-selector"
+              className={styles.fileSelector}
+              onClick={handleFileSelectorClick}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className={styles.attachIcon}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m18.375 12.739-7.693 7.693a4.5 4.5 0 0 1-6.364-6.364l10.94-10.94A3 3 0 1 1 19.5 7.372L8.552 18.32m.009-.01-.01.01m5.699-9.941-7.81 7.81a1.5 1.5 0 0 0 2.112 2.13"
+                />
+              </svg>
+              <input
+                id="file-element"
+                type="file"
+                ref={fileInputRef}
+                onChange={handleFileChange}
+                className="hidden"
+                hidden={true}
+              />
+              <span id="selected-filename" className={styles.selectedFilename}>
+                {selectedFileName}
+              </span>
+            </div>
+            <button type="submit" className={styles.submitButton}>
+              Submit
+            </button>
+          </div>
+        </div>
       </Form>
     </div>
   );
--- src/pages/GeneLists/single-list-viewer/SingleGeneListViewer.tsx
+++ src/pages/GeneLists/single-list-viewer/SingleGeneListViewer.tsx
@@ -1,10 +1,23 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useAppStore } from "../../../lib/state";
+import {
+  AnnotationsRequest,
+  AnnotationsResponse,
+  GeneAnnotation,
+  post,
+} from "../../../lib/api";
+import { TrashIcon } from "../../../assets/icons/Trash";
+
+import styles from "./SingleGeneListViewer.module.css";
 
 export const SingleGeneListViewer = ({ id }: Record<string, string>) => {
   const availableGeneLists = useAppStore((state) => state.availableGeneLists);
   const activeGeneList = useAppStore((state) => state.activeGeneList);
+  const selectedSpecies = useAppStore((state) => state.species);
   const setActiveGeneList = useAppStore((state) => state.setActiveGeneList);
+  const updateGeneList = useAppStore((state) => state.updateGeneList);
+
+  const [annotations, setAnnotations] = useState<Array<GeneAnnotation>>([]);
 
   useEffect(() => {
     // update lastAccessed in Gene List for this route
@@ -12,22 +25,84 @@ export const SingleGeneListViewer = ({ id }: Record<string, string>) => {
       (value) => value.id === id
     );
 
-    if (filterForActiveGeneList.length > 0) {
-      setActiveGeneList(filterForActiveGeneList[0].id);
-    }
+    if (filterForActiveGeneList.length < 0) return;
+
+    const geneList = filterForActiveGeneList[0];
+    setActiveGeneList(geneList.id);
+
+    const fetchAnnotations = async () => {
+      return await post<AnnotationsRequest, AnnotationsResponse>(
+        "/api/annotations",
+        {
+          species: selectedSpecies,
+          geneIds: geneList.geneIds,
+        }
+      );
+    };
+
+    fetchAnnotations()
+      .then((results) => setAnnotations(results.results))
+      .catch((error) => console.log(error));
   }, [id, availableGeneLists, setActiveGeneList]);
+
+  const deleteHandler = (geneIdToDelete: string) => {
+    return () => {
+      if (!activeGeneList) return;
+
+      updateGeneList({
+        ...activeGeneList,
+        geneIds: activeGeneList.geneIds.filter(
+          (value) => value !== geneIdToDelete
+        ),
+        updatedAt: new Date().toUTCString(),
+      });
+    };
+  };
 
   if (activeGeneList !== undefined) {
     return (
-      <div id="container">
-        <h2>
-          {activeGeneList.name} created {activeGeneList.createdAt}
-        </h2>
-        <ol>
-          {activeGeneList.geneIds.map((value, index) => (
-            <li key={index}>{value}</li>
-          ))}
-        </ol>
+      <div id="container" className={styles.geneListViewer}>
+        <div className={styles.geneListInfoDisplay}>
+          <span style={{ fontWeight: "bold" }}>
+            {activeGeneList.name} ({activeGeneList.geneIds.length} genes)
+          </span>
+          <span style={{ fontSize: "0.85em" }}>
+            created: {activeGeneList.createdAt}
+          </span>
+        </div>
+        <div style={{ width: "100%", overflowX: "auto", display: "grid" }}>
+          <table>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Delete?</th>
+              </tr>
+            </thead>
+            <tbody>
+              {annotations.length > 0
+                ? annotations.map((value, index) => (
+                    <tr key={index}>
+                      <td>{`${value.chromosomeId}_${value.geneId}`}</td>
+                      <td>{value.preferredName}</td>
+                      <td>{value.description}</td>
+                      <td>
+                        <button
+                          className={styles.deleteButton}
+                          onClick={deleteHandler(
+                            `${value.chromosomeId}_${value.geneId}`
+                          )}
+                        >
+                          <TrashIcon height={20} width={20} />
+                        </button>
+                      </td>
+                    </tr>
+                  ))
+                : null}
+            </tbody>
+          </table>
+        </div>
       </div>
     );
   }
